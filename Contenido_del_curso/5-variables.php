fb6ddcb4936b2d36c9a3e6117ce6cfebfd573848
jhasmany_jhunnior@outlook.com <jhasmany>
2020-08-21 21:05:38 -0400

    probar

--- Contenido_del_curso/5-variables.php
+++ Contenido_del_curso/5-variables.php
@@ -2,14 +2,30 @@
 //Variables de tipo entero
 $numero =10;
 echo 'Esto es un numero;'.$numero;
-//***********************************/
-echo '<br></br>'
-//Variables de tipo texto
+//***********************************//
+echo '<br></br>';
 
+//Variables de tipo texto
 $texto = "Esto es una cadena de texto";
 echo 'Mi cadena: '.$texto;
 
+//***********************************//
+echo '<br></br>';
 
+//Variable tipo booleam
+$booleam = true;
+echo 'Esto es una variable booleam: '.$booleam;
+//***********************************//
+echo '<br></br>';
 
-//***********************************/
+//Variable de tipo arreglo
+$arreglo = array("Ajejandro", "Maria");
+echo $arreglo[1];
+//***********************************//
+echo '<br></br>';
+
+//Variables de tipo arreglo con propiedades
+
+//***********************************//
+echo '<br></br>';
 ?>
