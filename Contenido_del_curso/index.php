33dec051c2d445389c471c40d84fd7a5378fe499
jhasmany_jhunnior@outlook.com <jhasmany>
2020-08-21 21:17:34 -0400

    insertardon

--- Contenido_del_curso/index.php
+++ Contenido_del_curso/index.php
@@ -1,3 +1,13 @@
-<?php
-echo "hola";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require("/Contenido_del_curso/5-variables");
+    ?>
+</body>
+</html>
